8c2e04a9e14230e7013808d9337f5d5b3a5b066a
Nick <19955751+nichnet@users.noreply.github.com>
2022-12-19 01:11:45 +0900

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+QUAD_SIZE = 8
+
+PALETTE = {
+    0: (255, 255, 255), 
+    1: (185, 185, 185),
+    2: (95, 95, 95),
+    3: (0, 0, 0)
+}
+
+SPRITE_SIZES = {
+    1: [QUAD_SIZE, QUAD_SIZE], 
+    2: [QUAD_SIZE, QUAD_SIZE * 2],
+    4: [QUAD_SIZE * 2, QUAD_SIZE * 2],
+    16: [QUAD_SIZE * 4, QUAD_SIZE* 4]
+}
